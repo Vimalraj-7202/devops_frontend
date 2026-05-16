f0585e0d857219209513f9b37c57ac702e84a2ca
Vimalraj S <vimalraj.s@hepl.com>
2026-05-16 16:05:35 +0530

    App.tsx file touched

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '20vh'
+      }}
+    >
+      <h1>My DevOps Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
